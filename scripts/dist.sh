307aa65902dcced774f0cd3e92a476905cbc75f7
Carter McKinnon <cartermckinnon@gmail.com>
2021-02-06 22:47:24 -0800

    Improve distribution script.

--- scripts/dist.sh
+++ scripts/dist.sh
@@ -8,6 +8,8 @@ cd ..
 rm -rf dist/
 mkdir dist/
 
+VERSION=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
+
 cp -R target/lib dist/
 cp target/url-alias-service-*.jar dist/
 cp -R target/site/jacoco dist/code-coverage
@@ -15,3 +17,6 @@ cp -R target/site/apidocs dist/javadoc
 cp configuration.yaml dist/
 cp scripts/run-dist.sh dist/run.sh
 cp -R k8s/ dist/
+
+cd dist/
+tar -czvf ../url-alias-service-$VERSION.tar.gz .
